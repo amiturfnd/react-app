f41383d5926a53a7ae5b5a79b7a9713e24416bd9
Amit Singh <amiturfnd@gmail.com>
2020-06-23 21:55:57 +0530

    Updates

---
+++ precache-manifest.c08f6db92c5691c21f6e2b8e4c58086b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f65171f9bd1d381b889288c1da6fb89",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "8a37ed78f57a12006b97",
+    "url": "/react-app/static/css/main.78071d66.chunk.css"
+  },
+  {
+    "revision": "a0d018d1f67db38f47ad",
+    "url": "/react-app/static/js/2.61ed92b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.61ed92b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a37ed78f57a12006b97",
+    "url": "/react-app/static/js/main.f022a8e2.chunk.js"
+  },
+  {
+    "revision": "8a98aa3f23d19ff99e11",
+    "url": "/react-app/static/js/runtime-main.cc0824c1.js"
+  },
+  {
+    "revision": "8df4d376059a5db0f38355d4d9215d5b",
+    "url": "/react-app/static/media/logo-white.8df4d376.png"
+  },
+  {
+    "revision": "cef50ee0d88def554c9aa661ad1a09d6",
+    "url": "/react-app/static/media/people-planning.cef50ee0.jpg"
+  },
+  {
+    "revision": "8cbb0f35a22185f81beeb97af0800e57",
+    "url": "/react-app/static/media/people-working.8cbb0f35.jpg"
+  },
+  {
+    "revision": "67fdf077b145c9583058fecb350c269f",
+    "url": "/react-app/static/media/search-24px.67fdf077.svg"
+  }
+]);
